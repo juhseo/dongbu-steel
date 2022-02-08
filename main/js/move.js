2480806094004ffc0f7d51945faca05c6fd0570b
juh <juhsseo@gmail.com>
2022-02-08 11:03:26 +0900

    main 텍스트 모션 완료

--- main/js/move.js
+++ main/js/move.js
@@ -1,14 +1,34 @@
+//sec02 텍스트 무빙
 $(document).ready(function(){
     $(window).on('scroll',function(){
         let scrollT = $(this).scrollTop();
-        let divTop = $('.txt_top').offset().top;
-        if(scrollT>divTop - 300){
-            $('.txt_top').addClass('on');
+        let divTop = $('.pos_box01').offset().top;
+        if(scrollT>divTop - 980){
+            $('.pos_box01').addClass('on');
         }else{
-            $('.txt_top').removeClass('on');
+            $('.pos_box01').removeClass('on');
         }
     })
 })
 
 //부모요소에 relative를 주고 움직일 대상에 absolute를 주고
 // top값 조정 opacity 조정
+
+
+//sec03 텍스트 무빙
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+        let scrollT = $(this).scrollTop();
+        let divTop = $('.pos_box02').offset().top;
+        if(scrollT>divTop - 980){
+            $('.pos_box02').addClass('on');
+        }else{
+            $('.pos_box02').removeClass('on');
+        }
+    })
+})
+
+//sec04 background image scale 조정
+$(document).ready(function(){
+    
+})
